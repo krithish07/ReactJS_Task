3eae1cb809b2f69795aa1f5a8ac50ca54b900f68
krithish <145008597+krithish07@users.noreply.github.com>
2024-08-11 00:32:08 +0530

    Add files via upload

---
+++ Task3/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './style1.css';
+// import Personaldetails from "./Components/About";
+
+import Task3 from './Components/Task3';
+
+function App() {
+  return (
+    
+   
+    <>
+    <Task3/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Task3/Task3.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function Task3(){
+    return(
+        <div>
+            <h1>Social Buttons</h1>
+            <button type="button"  className="btn btn-primary-m5">like</button>
+            <button type="button" className="btn1 btn-success-m5">command</button>
+            <button type="button" className="btn2 btn-warning-m5">share</button>
+            </div>
+            
+    )
+}
+
+export default Task3;
