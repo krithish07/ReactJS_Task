d9410bf65bbedeb7514bc127fe9642bae876a90d
krithish <145008597+krithish07@users.noreply.github.com>
2024-08-10 21:56:07 +0530

    Add files via upload

---
+++ About/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Img from '../Images/krithish.jpg';
+
+function About(){
+return(
+
+    <>
+  
+  <div className="Container">
+    <h1>Congratulations</h1>
+    <div className="insideCard">
+      <img src={Img} className="sarath"/>
+      <h3> krithish</h3>
+      <p>BSC Computer science</p>
+      <p>KG college of arts and science,saravanampatti</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+
+    </>
+)
+
+}
+export default About;
---
+++ About/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './style.css';
+// import './App.css';
+// import Personaldetails from "./Components/About";
+import About from './Components/About';
+function App() {
+  return (
+    
+   
+    <>
+    <About />
+    
+    </>
+  );
+}
+
+export default App;
