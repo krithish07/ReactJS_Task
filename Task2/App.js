1aa1828ab372471cacba6c8c5b92dae6eb6b0fbd
krithish <145008597+krithish07@users.noreply.github.com>
2024-08-10 23:24:40 +0530

    Add files via upload

---
+++ Task2/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './style.css';
+// import Personaldetails from "./Components/About";
+import Task2 from './Components/Task2';
+
+function App() {
+  return (
+    
+   
+    <>
+    <Task2/>
+    
+    </>
+  );
+}
+
+export default App;
