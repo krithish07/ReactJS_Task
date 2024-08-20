2fe15ab09d91da75f3695ddc4081e630cd94b412
krithish <145008597+krithish07@users.noreply.github.com>
2024-08-20 18:28:17 +0530

    Add files via upload

---
+++ Task2/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../src/App1.css";
+import Task2 from './Components/Task2';
+
+
+
+
+function App() {
+  return (
+    <>
+    
+      <Task2/> 
+    </>
+  );
+}
+
+export default App;
---
+++ Task2/Components/Task2.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Img from '../Images/MI.png';
+import moon from "../Images/rcb.webp";
+
+function Task2(){
+return(
+
+    <>
+  
+  <div className="Container">
+    <div className="insideCard">
+        <h1>INDIAN SUPER LEAGUE</h1>
+      <img src={Img} className="sarath"/>
+      <img src={moon} className="monish"/>
+    </div>
+  </div>
+
+    </>
+)
+
+}
+export default Task2;
