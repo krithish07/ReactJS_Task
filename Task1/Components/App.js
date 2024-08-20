19c6a7bb7c0abc792bb4b1c33310de8f61317dcb
krithish <145008597+krithish07@users.noreply.github.com>
2024-08-20 18:27:40 +0530

    Add files via upload

---
+++ Task1/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Task6 from './Components/Task6'; // Ensure the path is correct
+import "../src/style.css";
+
+// import './Components/App.css';
+// import Task5 from './Components/Task5';
+import Task1 from './Components/Task1';
+function App() {
+  return (
+    <>
+    
+      <Task1/> 
+    </>
+  );
+}
+
+export default App;
